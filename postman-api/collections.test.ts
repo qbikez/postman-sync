92cc0bc8cbde7cde8cdfa31dae2e6474e7f5ae59
Jakub Pawlowski <jakub.pawlowski@guestline.com>
2020-03-17 05:43:50 +0100

    create, update, delete

--- postman-api/collections.test.ts
+++ postman-api/collections.test.ts
@@ -1,23 +1,46 @@
 import { PostmanApi } from "./collections";
+import uuid from "uuid";
 
 const apiKey = process.env.POSTMAN_API_KEY || "";
 
 describe("postman API", () => {
-    const postmanApi = new PostmanApi(apiKey);
-  
-    describe("when calling GET collections", () => {
-      it("should get all collections", async () => {
-        const collections = await postmanApi.getCollections();
-        expect(collections.length).toBeGreaterThan(0);
-      });
-  
-      it("should get a single collection", async () => {
-        const collections = await postmanApi.getCollections();
-        const uuid = collections[0].uid;
-        const collection = await postmanApi.getCollection(uuid);
-  
-        expect(collection).not.toBeNull();
-        expect(collection.info._postman_id).not.toBeNull();
-      });
+  const postmanApi = new PostmanApi(apiKey);
+
+  describe("when calling GET collections", () => {
+    it("should get all collections", async () => {
+      const collections = await postmanApi.getCollections();
+      expect(collections.length).toBeGreaterThan(0);
+    });
+
+    it("should get a single collection", async () => {
+      const collections = await postmanApi.getCollections();
+      const uuid = collections[0].uid;
+      const collection = await postmanApi.getCollection(uuid);
+
+      expect(collection).not.toBeNull();
+      expect(collection.info._postman_id).not.toBeNull();
     });
   });
+
+  describe("create collection", () => {
+    const name = `test-collection-${uuid.v4()}`;
+    let id: string;
+    it("should work", async () => {
+      const resp = await postmanApi.createCollection({
+        info: {
+          name
+        },
+        item: []
+      });
+
+      id = resp.id;
+      expect(resp.name).toBe(name);
+    });
+
+    afterAll(async () => {
+      if (id) {
+        await postmanApi.deleteCollection(id);
+      }
+    });
+  });
+});
